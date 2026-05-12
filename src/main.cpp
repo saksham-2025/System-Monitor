207fbc49db6724f98f1613ac918e39e60125f3cb
Saksham Kumar Gupta <sakshamk.mca25@cs.du.ac.in>
2026-05-12 11:05:25 +0530

    Added interactive ncurses controls and dynamic process sorting

--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,12 @@ using namespace std ;
 
 int main(){
 initscr();
+keypad(stdscr, TRUE);
+timeout(100);
+bool sortByCPU= true ;
 while (true){
     clear();
+    mvprintw(0, 2,"Press q to quit | c = CPU sort | m = Memory sort");
     auto data1 = getCPUData();
     vector<Process> processes = takeProcessSnapshot();
     sleep(1);
@@ -20,11 +24,29 @@ while (true){
     calculateProcessCpuUsage(processes , data2.first - data1.first);
     double cpuUsage = getCpuUsage(data1 ,data2);
     double memUsage=  getMemUsage();
+    // sort(processes.begin(),processes.end(),compareByCPU);
+    if(sortByCPU)
+{
+    sort(processes.begin(), processes.end(), compareByCPU);
+    mvprintw(22 ,2 ,"SORT BY CPU");
+}
+else
+{
+    sort(processes.begin(), processes.end(), compareBYMemory);
+    mvprintw(22 ,2 ,"SORT BY MEMORY");
+    
+}
     renderSystemStats(cpuUsage ,memUsage);
-    sort(processes.begin(),processes.end(),compareByCPU);
     renderProcessTable(processes,10);
     refresh() ;
-    
+    int ch = getch();
+    if(ch == 'q') break ;
+    else if(ch=='c') {
+        sortByCPU =true ;
+    }
+    else if(ch=='m'){
+        sortByCPU = false ;
+    }    
 }
 endwin();
  
